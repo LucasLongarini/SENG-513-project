e9e4297475f443dbf480ce40ccd12a04948fa441
LucasLongarini <lucaslongarini0@gmail.com>
2020-11-13 21:34:08 -0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import LoginView from './Views/LoginView/LoginView';
 
 function App() {
   return (
-    <Button variant="contained">
-        Hello World
-    </Button>
+    <LoginView></LoginView>
   );
 }
 
---
+++ client/src/Views/LoginView/LoginView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import './LoginView.css';
+
+function LoginView() {
+  return (
+    <div className='login-view'>
+        <Container className='login-container' maxWidth='sm'>
+        </Container>
+    </div>
+  );
+}
+
+export default LoginView;
