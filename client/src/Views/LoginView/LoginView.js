d7fcbdbd420713baf4345d3baf062af519e06d04
LucasLongarini <lucaslongarini0@gmail.com>
2020-11-15 01:07:58 -0700

    .

--- client/src/Views/LoginView/LoginView.js
+++ client/src/Views/LoginView/LoginView.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 import './LoginView.css';
 
 function LoginView() {
   return (
     <div className='login-view'>
         <Container className='login-container' maxWidth='sm'>
+          <TextField id="standard-basic" label="Standard" fullWidth/>
         </Container>
     </div>
   );
