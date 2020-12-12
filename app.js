7854dc5a7b67ad1f907cb0fda79608b9b95e98be
Lucas Longarini <37842688+LucasLongarini@users.noreply.github.com>
2020-12-11 22:01:28 -0700

    adjusted files for heroku deploy

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.use('/room', roomRoutes);
 
 
 // Serving web page
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get('/*', (req, res) => {
     //res.sendStatus(200);
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 
 // sockets
